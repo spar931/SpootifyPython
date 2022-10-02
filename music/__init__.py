af254ad1f3e25c8bd730f32d04aa98b45e96402d
spar931 <spar931@aucklanduni.ac.nz>
2022-10-02 14:32:21 +1300

    register callback in init.py

--- music/__init__.py
+++ music/__init__.py
@@ -109,4 +109,17 @@ def create_app(test_config=None):
         from .utilities import utilities
         app.register_blueprint(utilities.utilities_blueprint)
 
+        # Register a callback the makes sure that database sessions are associated with http requests
+        # We reset the session inside the database repository before a new flask request is generated
+        @app.before_request
+        def before_flask_http_request_function():
+            if isinstance(repo.repo_instance, database_repository.SqlAlchemyRepository):
+                repo.repo_instance.reset_session()
+
+        # Register a tear-down method that will be called after each request has been processed.
+        @app.teardown_appcontext
+        def shutdown_session(exception=None):
+            if isinstance(repo.repo_instance, database_repository.SqlAlchemyRepository):
+                repo.repo_instance.close_session()
+
     return app
